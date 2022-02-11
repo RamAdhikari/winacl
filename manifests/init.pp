7179a9c91daf00a8d51e7225804df0ec94affc90
Ram Adhikari <im_ram@hotmail.com>
2022-02-11 21:48:09 +1100

    corrected

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,8 +6,13 @@
 #   include winacl
 class winacl {
   exec { 'machinekayacl':
-  command   => file('winacl/winacl.ps1'),
+  command   => file('winacl/restore-machinekeys-acl.ps1'),
   unless    => 'if(((((((get-acl $TargetFolder).access)| select IdentityReference | ft -HideTableHeaders | Out-String).trim()) -replace "\`r\`n", "" ) -replace "              ",",") -ne "Everyone,BUILTIN\Administrators") { exit 1 }',
+  provider  => powershell,
+}
+exec { 'rename-guest':
+  command   => '(Get-WMIObject Win32_UserAccount -Filter "Name=\'guest\'").Rename("new-guest")',
+  unless    => 'if (Get-WmiObject Win32_UserAccount -Filter "Name=\'guest\'") { exit 1 }',
   provider  => powershell,
 }
 }
